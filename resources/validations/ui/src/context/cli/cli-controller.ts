cdcd54e39d2910a48176a8d41aecfa3f9707c6c7
Daniel Naab <dan@crushingpennies.com>
2021-06-10 09:31:48 -0500

    In-progress CLI changes with commander, working through sync file issues

--- resources/validations/ui/src/context/cli/cli-controller.ts
+++ resources/validations/ui/src/context/cli/cli-controller.ts
@@ -1,3 +1,27 @@
+import { readFileSync } from 'fs';
+
+import { Command } from 'commander';
+import type { ValidateSchematronUseCase } from 'src/use-cases/validate-ssp-xml';
+
+type CommandLineContext = {
+  validateSchematron: ValidateSchematronUseCase;
+};
+
+export const CommandLineController = (ctx: CommandLineContext) => {
+  const cli = new Command();
+  cli
+    .command('validate <ssp-xml-file>')
+    .description('validate OSCAL systems security plan document')
+    .action(sspXmlFile => {
+      const xmlString = readFileSync(sspXmlFile, 'utf-8');
+      ctx.validateSchematron(xmlString).then(validationReport => {
+        console.log(validationReport);
+      });
+    });
+  return cli;
+};
+
+/*
 import yargs from 'yargs';
 
 type CommandLineOptions = {
@@ -6,23 +30,26 @@ type CommandLineOptions = {
 };
 
 export const commandLineController = (ctx: CommandLineOptions) => {
-  return yargs(ctx.argv)
-    .exitProcess(ctx.exitProcess)
-    .usage('Usage: $0 <command> [options]')
-    .command('validate', 'Validate systems security plan', yargs => {
-      return yargs.option('ssp', {
-        type: 'string',
-        demand: 'Please specify OSCAL SSP file path',
-        nargs: 1,
-        describe: 'Systems security plan in OSCAL XML format',
-      });
-    })
-    .example('$0 validate --ssp my-fedramp-ssp.xml', 'Validate SSP')
+  return (
+    yargs(ctx.argv)
+      .exitProcess(ctx.exitProcess)
+      .usage('Usage: $0 <command> [options]')
+      .command('validate', 'Validate systems security plan', yargs => {
+        return yargs.option('ssp', {
+          type: 'string',
+          demand: 'Please specify OSCAL SSP file path',
+          nargs: 1,
+          describe: 'Systems security plan in OSCAL XML format',
+        });
+      })
+      //.example('$0 validate --ssp my-fedramp-ssp.xml', 'Validate SSP')
 
-    .demand(1, 'Please specify a command!')
-    .strict()
+      .demand(1, 'Please specify a command!')
+      .strict()
 
-    .help('h')
-    .alias('h', 'help')
-    .wrap(null);
+      .help('h')
+      .alias('h', 'help')
+      .wrap(null)
+  );
 };
+*/
--- resources/validations/ui/src/context/cli/index.ts
+++ resources/validations/ui/src/context/cli/index.ts
@@ -1,8 +1,19 @@
 #!/usr/bin/env -S ts-node --script-mode
 
-import { commandLineController } from './cli-controller';
+// @ts-ignore
+import * as SaxonJS from 'saxon-js';
 
-commandLineController({
-  argv: process.argv.slice(2),
-  exitProcess: true,
+import { SaxonJsSchematronValidatorGateway } from '../shared/saxon-js-gateway';
+import { CommandLineController } from './cli-controller';
+
+const controller = CommandLineController({
+  validateSchematron: SaxonJsSchematronValidatorGateway({
+    sefUrl: `file:///Users/dan/src/10x/fedramp-automation/resources/validations/ui/public/ssp.sef.json`,
+    // The npm version of saxon-js is for node; currently, we load the
+    // browser version via a script tag in index.html.
+    SaxonJS: SaxonJS,
+    registryBaseUrl: `file:///Users/dan/src/10x/fedramp-automation/resources/xml`,
+  }),
 });
+controller.parse(process.argv);
+console.log(controller.opts());
