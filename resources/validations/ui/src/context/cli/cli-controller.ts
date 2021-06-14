d4e29a9e4f3f5c9caa9f06937e9caddb2f1fb610
Daniel Naab <dan@crushingpennies.com>
2021-06-14 15:34:10 -0500

    Remove old yargs reference

--- resources/validations/ui/src/context/cli/cli-controller.ts
+++ resources/validations/ui/src/context/cli/cli-controller.ts
@@ -22,36 +22,3 @@ export const CommandLineController = (ctx: CommandLineContext) => {
     });
   return cli;
 };
-
-/*
-import yargs from 'yargs';
-
-type CommandLineOptions = {
-  argv: string[];
-  exitProcess: boolean;
-};
-
-export const commandLineController = (ctx: CommandLineOptions) => {
-  return (
-    yargs(ctx.argv)
-      .exitProcess(ctx.exitProcess)
-      .usage('Usage: $0 <command> [options]')
-      .command('validate', 'Validate systems security plan', yargs => {
-        return yargs.option('ssp', {
-          type: 'string',
-          demand: 'Please specify OSCAL SSP file path',
-          nargs: 1,
-          describe: 'Systems security plan in OSCAL XML format',
-        });
-      })
-      //.example('$0 validate --ssp my-fedramp-ssp.xml', 'Validate SSP')
-
-      .demand(1, 'Please specify a command!')
-      .strict()
-
-      .help('h')
-      .alias('h', 'help')
-      .wrap(null)
-  );
-};
-*/
