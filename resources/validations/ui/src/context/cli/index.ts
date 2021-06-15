b42c2c17ddf9779c180542599430762d93ecc1ef
Daniel Naab <dan@crushingpennies.com>
2021-06-15 09:11:57 -0500

    Remove hardcoded paths

--- resources/validations/ui/src/context/cli/index.ts
+++ resources/validations/ui/src/context/cli/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S ts-node --script-mode
 
+import * as path from 'path';
+
 // @ts-ignore
 import * as SaxonJS from 'saxon-js';
 
@@ -7,17 +9,24 @@ import { SaxonJsSchematronValidatorGateway } from '../shared/saxon-js-gateway';
 import { CommandLineController } from './cli-controller';
 import { readStringFile } from './file-gateway.humble';
 
+const PROJECT_ROOT = process.cwd();
+
 const controller = CommandLineController({
   readStringFile,
   validateSchematron: SaxonJsSchematronValidatorGateway({
-    sefUrl: `file:///Users/dan/src/10x/fedramp-automation/resources/validations/ui/public/ssp.sef.json`,
+    sefUrl: `file://${path.join(PROJECT_ROOT, 'public/ssp.sef.json')}`,
     // The npm version of saxon-js is for node; currently, we load the
     // browser version via a script tag in index.html.
     SaxonJS: SaxonJS,
-    baselinesBaseUrl: `file:///Users/dan/src/10x/fedramp-automation/baselines/rev4/xml`,
-    registryBaseUrl: `file:///Users/dan/src/10x/fedramp-automation/resources/xml`,
+    baselinesBaseUrl: `file://${path.join(
+      PROJECT_ROOT,
+      '../../../baselines/rev4/xml',
+    )}`,
+    registryBaseUrl: `file://${path.join(
+      PROJECT_ROOT,
+      '../../../resources/xml',
+    )}`,
   }),
 });
 
 controller.parse(process.argv);
-console.log(controller.opts());
