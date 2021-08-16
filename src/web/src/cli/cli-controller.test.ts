797aa6ac01a58986aaf8e3392b53bcd1a72f1463
Daniel Naab <dan@crushingpennies.com>
2021-08-16 17:07:17 -0500

    Fix failing test due to change of file i/o on a CLI command from sync to async.

--- src/web/src/cli/cli-controller.test.ts
+++ src/web/src/cli/cli-controller.test.ts
@@ -1,11 +1,11 @@
 import { CommandLineController } from './cli-controller';
 
 describe('command-line controller', () => {
-  it('calls validate schematron', () => {
+  it('calls validate schematron', async () => {
     const mockXml = '<xml></xml>';
     const ctx = {
       readStringFile: jest.fn().mockReturnValue(Promise.resolve(mockXml)),
-      writeStringFile: jest.fn(),
+      writeStringFile: jest.fn().mockReturnValue(Promise.resolve()),
       useCases: {
         parseSchematron: jest.fn(),
         validateSSP: jest.fn().mockReturnValue(
@@ -17,7 +17,7 @@ describe('command-line controller', () => {
       },
     };
     const cli = CommandLineController(ctx);
-    cli.parse(['ts-node', 'index.ts', 'validate', 'ssp.xml']);
+    await cli.parse(['ts-node', 'index.ts', 'validate', 'ssp.xml']);
     expect(ctx.readStringFile).toHaveBeenCalledWith('ssp.xml');
     expect(ctx.useCases.validateSSP).toHaveBeenCalledWith(mockXml);
   });
