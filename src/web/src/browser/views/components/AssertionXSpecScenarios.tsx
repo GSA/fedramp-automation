ae0932dca35ed01b16f23d15eb553572058b11ec
Daniel Naab <dan@crushingpennies.com>
2022-05-11 15:48:18 -0500

    Fix macOS filename casing problem.

--- src/web/src/browser/views/components/AssertionXSpecScenarios.tsx
+++ src/web/src/browser/views/components/AssertionXSpecScenarios.tsx
@@ -2,24 +2,19 @@ import React from 'react';
 
 import type { ScenarioSummary } from '@asap/shared/domain/xspec';
 import { CodeViewer } from './CodeViewer';
-import { ContentOverlay } from './ContentOverlay';
 
-type Props = {
+export const AssertionXSpecScenarios = (props: {
   scenarioSummaries: ScenarioSummary[];
-};
-
-export const AssertionXSpecScenarios = ({ scenarioSummaries }: Props) => {
+}) => {
   return (
-    <ContentOverlay>
-      <ul>
-        {scenarioSummaries.map((scenario, index) => (
-          <li key={index}>
-            {scenario.label}{' '}
-            <span className="text-red">{scenario.assertionLabel}</span>
-            <CodeViewer codeHTML={scenario.context}></CodeViewer>
-          </li>
-        ))}
-      </ul>
-    </ContentOverlay>
+    <ul>
+      {props.scenarioSummaries.map((scenario, index) => (
+        <li key={index}>
+          {scenario.label}{' '}
+          <span className="text-bold">{scenario.assertionLabel}</span>
+          <CodeViewer codeHTML={scenario.context}></CodeViewer>
+        </li>
+      ))}
+    </ul>
   );
 };
