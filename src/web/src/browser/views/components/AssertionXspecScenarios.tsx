2fa86a2d37ea59c34fb707a783ddaa5b578bbd86
Daniel Naab <dan@crushingpennies.com>
2022-05-11 15:39:37 -0500

    Add updated AssertionXSpecScenarios

--- src/web/src/browser/views/components/AssertionXspecScenarios.tsx
+++ src/web/src/browser/views/components/AssertionXspecScenarios.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import type { ScenarioSummary } from '@asap/shared/domain/xspec';
 import { CodeViewer } from './CodeViewer';
 
-type Props = {
+export const AssertionXSpecScenarios = (props: {
   scenarioSummaries: ScenarioSummary[];
-};
-
-export const AssertionXSpecScenarios = ({ scenarioSummaries }: Props) => {
+}) => {
   return (
     <ul>
-      {scenarioSummaries.map((scenario, index) => (
+      {props.scenarioSummaries.map((scenario, index) => (
         <li key={index}>
           {scenario.label}{' '}
           <span className="text-bold">{scenario.assertionLabel}</span>
