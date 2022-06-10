51c09c9c52009ef036ee26dcc213c50cd502e049
Daniel Naab <dan@crushingpennies.com>
2022-06-10 13:08:43 -0500

    Initial Vite config - still need asset handling to be worked out.

--- src/web/src/index.ts
+++ src/web/src/index.ts
@@ -1,8 +1,8 @@
 import { runBrowserContext } from '@asap/browser';
-
+console.log(import.meta.env);
 runBrowserContext({
   element: document.getElementById('root') as HTMLElement,
-  baseUrl: import.meta.env.BASEURL,
+  baseUrl: import.meta.env.BASE_URL,
   debug: false,
   deploymentId: import.meta.env.DEPLOYMENT_ID,
   importMetaHot: import.meta.hot,
