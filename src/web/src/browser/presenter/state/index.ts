6ce892b5cc34df507fbc69db1b40e83cb6a00232
Daniel Naab <dan@crushingpennies.com>
2022-06-20 13:26:19 -0500

    Create a constructor for machine reducers.

--- src/web/src/browser/presenter/state/index.ts
+++ src/web/src/browser/presenter/state/index.ts
@@ -1,9 +1,9 @@
 import * as assertionDocumentation from './assertion-documetation';
 import * as metrics from './metrics';
 import * as routerMachine from './router-machine';
-import * as schematronMachine from './schematron-machine';
-import * as validatorMachine from './validator-machine';
-import * as validationResultsMachine from './validation-results-machine';
+import * as schematron from './schematron-machine';
+import * as validator from './validator-machine';
+import * as validationResults from './validation-results-machine';
 
 export type State = {
   config: {
@@ -17,23 +17,23 @@ export type State = {
   assertionDocumentation: assertionDocumentation.State;
   metrics: metrics.State;
   oscalDocuments: {
-    poam: schematronMachine.State;
-    sap: schematronMachine.State;
-    sar: schematronMachine.State;
-    ssp: schematronMachine.State;
+    poam: schematron.State;
+    sap: schematron.State;
+    sar: schematron.State;
+    ssp: schematron.State;
   };
   router: routerMachine.State;
-  validator: validatorMachine.State;
+  validator: validator.State;
   validationResults: {
-    poam: validationResultsMachine.State;
-    sap: validationResultsMachine.State;
-    sar: validationResultsMachine.State;
-    ssp: validationResultsMachine.State;
+    poam: validationResults.State;
+    sap: validationResults.State;
+    sar: validationResults.State;
+    ssp: validationResults.State;
   };
 };
 
 type ScopedTransition = {
-  machine: string;
+  machine: keyof typeof reducers;
 };
 
 export type StateTransition =
@@ -42,35 +42,10 @@ export type StateTransition =
         | assertionDocumentation.StateTransition
         | metrics.StateTransition
         | routerMachine.StateTransition
-        | schematronMachine.StateTransition
-        | validatorMachine.StateTransition
-        | validationResultsMachine.StateTransition
+        | schematron.StateTransition
+        | validator.StateTransition
+        | validationResults.StateTransition
       );
-
-/*type BaseReducer = (state: State, event: StateTransition) => State;
-type Reducer<S, ST> = (state: S, event: ST) => S;
-
-type ReducerParts = BaseReducer extends (
-  state: infer MachineState,
-  event: infer MachineStateTransition,
-) => infer MachineState
-  ? { State: MachineState; StateTransition: MachineStateTransition }
-  : never;
-
-type MachineKey = string;
-const reduceMachine = <R extends ReducerParts>(
-  machine: string,
-  state: R['State'],
-  reducer: Reducer<R['State'], R['StateTransition']>,
-) => {
-  return (state: R['State'], event: R['StateTransition']) => {
-    if (event.machine === machine) {
-      return reducer(state, event);
-    }
-    return state;
-  };
-};
-*/
 
 export const initialState: State = {
   config: {
@@ -82,112 +57,99 @@ export const initialState: State = {
   assertionDocumentation: assertionDocumentation.initialState,
   metrics: metrics.initialState,
   oscalDocuments: {
-    poam: schematronMachine.initialState,
-    sap: schematronMachine.initialState,
-    sar: schematronMachine.initialState,
-    ssp: schematronMachine.initialState,
+    poam: schematron.initialState,
+    sap: schematron.initialState,
+    sar: schematron.initialState,
+    ssp: schematron.initialState,
   },
   router: routerMachine.initialState,
-  validator: validatorMachine.initialState,
+  validator: validator.initialState,
   validationResults: {
-    poam: validationResultsMachine.initialState,
-    sap: validationResultsMachine.initialState,
-    sar: validationResultsMachine.initialState,
-    ssp: validationResultsMachine.initialState,
+    poam: validationResults.initialState,
+    sap: validationResults.initialState,
+    sar: validationResults.initialState,
+    ssp: validationResults.initialState,
   },
 };
 
+const createMachineReducer =
+  <S, ST>(machine: string, nextState: (s: S, e: ST) => S) =>
+  (state: S, event: StateTransition) => {
+    if (event.machine === machine) {
+      return nextState(state, event as unknown as ST);
+    }
+    return state;
+  };
+
+const reducers = {
+  assertionDocumentation: createMachineReducer(
+    'assertionDocumentation',
+    assertionDocumentation.nextState,
+  ),
+  metrics: createMachineReducer('metrics', metrics.nextState),
+  'oscalDocuments.poam': createMachineReducer(
+    'oscalDocuments.poam',
+    schematron.nextState,
+  ),
+  'oscalDocuments.sap': createMachineReducer(
+    'oscalDocuments.sap',
+    schematron.nextState,
+  ),
+  'oscalDocuments.sar': createMachineReducer(
+    'oscalDocuments.sar',
+    schematron.nextState,
+  ),
+  'oscalDocuments.ssp': createMachineReducer(
+    'oscalDocuments.ssp',
+    schematron.nextState,
+  ),
+  router: createMachineReducer('router', routerMachine.nextState),
+  'validationResults.poam': createMachineReducer(
+    'validationResults.poam',
+    validationResults.nextState,
+  ),
+  'validationResults.sap': createMachineReducer(
+    'validationResults.sap',
+    validationResults.nextState,
+  ),
+  'validationResults.sar': createMachineReducer(
+    'validationResults.sar',
+    validationResults.nextState,
+  ),
+  'validationResults.ssp': createMachineReducer(
+    'validationResults.ssp',
+    validationResults.nextState,
+  ),
+  validator: createMachineReducer('validator', validator.nextState),
+} as const;
+
 export const rootReducer = (state: State, event: StateTransition): State => ({
   config: state.config,
-  assertionDocumentation: assertionDocumentation.nextState(
+  assertionDocumentation: reducers.assertionDocumentation(
     state.assertionDocumentation,
-    event as assertionDocumentation.StateTransition,
+    event,
   ),
-  metrics: metrics.nextState(state.metrics, event as metrics.StateTransition),
+  metrics: reducers.metrics(state.metrics, event),
   oscalDocuments: {
-    poam: ((state: schematronMachine.State, event: StateTransition) => {
-      if (event.machine === 'oscalDocuments.poam') {
-        return schematronMachine.nextState(
-          state,
-          event as unknown as schematronMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.oscalDocuments.poam, event),
-    sap: ((state: schematronMachine.State, event: StateTransition) => {
-      if (event.machine === 'oscalDocuments.sap') {
-        return schematronMachine.nextState(
-          state,
-          event as unknown as schematronMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.oscalDocuments.sap, event),
-    sar: ((state: schematronMachine.State, event: StateTransition) => {
-      if (event.machine === 'oscalDocuments.poam') {
-        return schematronMachine.nextState(
-          state,
-          event as unknown as schematronMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.oscalDocuments.sar, event),
-    ssp: ((state: schematronMachine.State, event: StateTransition) => {
-      if (event.machine === 'oscalDocuments.ssp') {
-        return schematronMachine.nextState(
-          state,
-          event as unknown as schematronMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.oscalDocuments.ssp, event),
+    poam: reducers['oscalDocuments.poam'](state.oscalDocuments.poam, event),
+    sap: reducers['oscalDocuments.sap'](state.oscalDocuments.sap, event),
+    sar: reducers['oscalDocuments.sar'](state.oscalDocuments.sar, event),
+    ssp: reducers['oscalDocuments.ssp'](state.oscalDocuments.ssp, event),
   },
   router: routerMachine.nextState(
     state.router,
     event as routerMachine.StateTransition,
   ),
-  validator: validatorMachine.nextState(
-    state.validator,
-    event as validatorMachine.StateTransition,
-  ),
   validationResults: {
-    poam: ((state: validationResultsMachine.State, event: StateTransition) => {
-      if (event.machine === 'validationResults.poam') {
-        return validationResultsMachine.nextState(
-          state,
-          event as unknown as validationResultsMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.validationResults.poam, event),
-    sap: ((state: validationResultsMachine.State, event: StateTransition) => {
-      if (event.machine === 'validationResults.sap') {
-        return validationResultsMachine.nextState(
-          state,
-          event as unknown as validationResultsMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.validationResults.sap, event),
-    sar: ((state: validationResultsMachine.State, event: StateTransition) => {
-      if (event.machine === 'validationResults.poam') {
-        return validationResultsMachine.nextState(
-          state,
-          event as unknown as validationResultsMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.validationResults.sar, event),
-    ssp: ((state: validationResultsMachine.State, event: StateTransition) => {
-      if (event.machine === 'validationResults.ssp') {
-        return validationResultsMachine.nextState(
-          state,
-          event as unknown as validationResultsMachine.StateTransition,
-        );
-      }
-      return state;
-    })(state.validationResults.ssp, event),
+    poam: reducers['validationResults.poam'](
+      state.validationResults.poam,
+      event,
+    ),
+    sap: reducers['validationResults.sap'](state.validationResults.poam, event),
+    sar: reducers['validationResults.sar'](state.validationResults.sar, event),
+    ssp: reducers['validationResults.ssp'](state.validationResults.ssp, event),
   },
+  validator: reducers.validator(state.validator, event),
 });
 
 export type SampleDocument = {
