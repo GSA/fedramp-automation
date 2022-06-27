3ff1a8ba68f3bc602649e3a85098c2e409189b5e
Daniel Naab <dan@crushingpennies.com>
2022-06-27 16:44:56 -0500

    Clean up OSCAL build makefile.

--- src/content/module.mk
+++ src/content/module.mk
@@ -8,17 +8,15 @@ MNT := /code
 OSCAL_DIR := vendor/oscal
 CICD_DIR_PATH := $(OSCAL_DIR)/build/ci-cd
 CONTENT_CONFIG_PATH := src/config
-DIST_PATH := dist
 
 OSCAL_DOCKER := docker-compose -f $(OSCAL_DIR)/build/docker-compose.yml -f $(CONTENT_DIR)/docker-compose.yml
 
 init-content:
+  @echo "Building OSCAL content generator OSCAL Docker image..."
 	$(OSCAL_DOCKER) build
 
 test-content:  ## Test src/content
 	@echo "Testing content..."
-	#"$(CICD_DIR_PATH)/validate-content.sh" -v -o "$(OSCAL_DIR)" -a . -c "$(CONTENT_CONFIG_PATH)"
-	#docker run -v $(PWD):$(MNT) oscal-builder "$(MNT)/$(CICD_DIR_PATH)/validate-content.sh -v -o $(MNT)/$(OSCAL_DIR_PATH) -a $(MNT) -c $(MNT)/$(CONTENT_CONFIG_PATH)"
 	$(OSCAL_DOCKER) run cli $(MNT)/$(CICD_DIR_PATH)/validate-content.sh -v -o $(MNT)/$(OSCAL_DIR) -a $(MNT) -c $(MNT)/$(CONTENT_CONFIG_PATH)
 
 build-content:  ## Build dist/content
