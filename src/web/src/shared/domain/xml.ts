dfe3c5c2deccbd425d328ffcc7a22839bdce914d
Daniel Naab <dan@crushingpennies.com>
2022-07-22 09:23:44 -0500

    Add function to start and end line numbers for xml elements.

--- src/web/src/shared/domain/xml.ts
+++ src/web/src/shared/domain/xml.ts
@@ -1,2 +1,48 @@
 export type FormatXml = (xml: string) => string;
 export type IndentXml = (xml: string) => Promise<string>;
+
+type LineRange = { start: number; end: number } | null;
+export const getLineNumbersForElementById = (xml: string, element: string) => {
+  const regEx = new RegExp(
+    `<${element}[^]*?\\sid=\\"([^\\"]+)\\"[^]*?</${element}>`,
+    'g',
+  );
+  const matches = xml.matchAll(regEx);
+
+  const lineNumbers: Record<string, LineRange> = {};
+  for (const match of matches) {
+    const elementString = match[0];
+    const id = match[1];
+    lineNumbers[id] = linesOf(xml, elementString);
+  }
+  return lineNumbers;
+};
+
+const linesOf = (text: string, substring: string) => {
+  let line = 0;
+  let matchLine = 0;
+  let matchedChars = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === substring[matchedChars]) {
+      matchedChars++;
+    } else {
+      matchedChars = 0;
+      matchLine = 0;
+    }
+    if (matchedChars === substring.length) {
+      return {
+        start: line - matchLine,
+        end: line,
+      };
+    }
+    if (text[i] === '\n') {
+      line++;
+      if (matchedChars > 0) {
+        matchLine++;
+      }
+    }
+  }
+
+  return null;
+};
