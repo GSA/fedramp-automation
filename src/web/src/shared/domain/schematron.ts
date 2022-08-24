3e1471d71147637357aaa32cbb02292af40e038f
Daniel Naab <dan@crushingpennies.com>
2022-08-24 09:31:08 -0500

    Fix merge issue, due to moving files around

--- src/web/src/shared/domain/schematron.ts
+++ src/web/src/shared/domain/schematron.ts
@@ -38,6 +38,11 @@ export type SchematronJSONToXMLProcessor = (
   jsonString: string,
 ) => Promise<string>;
 
+export type SchematronJSONToXMLProcessors = Record<
+  OscalDocumentKey,
+  SchematronJSONToXMLProcessor
+>;
+
 export type SchematronProcessor = (oscalXmlString: string) => Promise<{
   documentType: OscalDocumentKey;
   schematronResult: SchematronResult;
