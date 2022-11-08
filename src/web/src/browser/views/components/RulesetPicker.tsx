666ebdbba48c60b74d497de200727165e383cc63
Daniel Naab <dan@crushingpennies.com>
2022-11-08 15:35:03 -0600

    Update RulesetPicker to change URL on select

--- src/web/src/browser/views/components/RulesetPicker.tsx
+++ src/web/src/browser/views/components/RulesetPicker.tsx
@@ -1,7 +1,8 @@
 import { useAppContext } from '../context';
-import * as validator from '../../presenter/actions/validator';
 import '../styles/RulesetPicker.scss';
 import { SchematronRulesetKey } from '@asap/shared/domain/schematron';
+import { setCurrentRoute } from '@asap/browser/presenter/actions';
+import { getUrl, Routes } from '@asap/browser/presenter/state/router';
 
 export const RulesetPicker = () => {
   const { dispatch, state } = useAppContext();
@@ -11,27 +12,29 @@ export const RulesetPicker = () => {
         <div className="grid-col-auto">
           <span className="usa-accordion">
             <select
-              className="usa-select margin-right-105"
+              className="usa-select margin-right-105 border-0 padding-top-0 padding-bottom-0 margin-top-0"
               name="schematron-ruleset"
               id="schematron-ruleset"
               disabled={state.validator.current === 'PROCESSING'}
               onChange={event => {
-                  dispatch(
-                    validator.setSchematronRuleset(
-                      event.target.options[event.target.selectedIndex].value as SchematronRulesetKey,
+                dispatch(
+                  setCurrentRoute(
+                    getUrl(
+                      Routes.documentSummary(
+                        event.target.options[event.target.selectedIndex]
+                          .value as SchematronRulesetKey,
+                      ),
                     ),
-                  );
+                  ),
+                );
               }}
             >
               {Object.entries(state.validator.ruleset.choices).map(
                 ([rulesetKey, ruleset], index) => (
-                  <option
-                    key={index}
-                    value={rulesetKey}
-                  >
+                  <option key={index} value={rulesetKey}>
                     {ruleset.title}
                   </option>
-                )
+                ),
               )}
             </select>
           </span>
