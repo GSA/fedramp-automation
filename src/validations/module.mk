fc3c0af431a7ac4482e7c4c718729f6ec7797330
~ . ~ <paul.n.wand@gsa.gov>
2024-07-19 13:31:27 -0400

    Update module.mk

--- src/validations/module.mk
+++ src/validations/module.mk
@@ -2,6 +2,8 @@
 OSCAL_CLI = oscal
 SRC_DIR = ./src
 DIST_DIR = ./dist
+REV5_BASELINES = ./dist/content/rev5/baselines
+REV5_TEMPLATES = ./dist/content/rev5/templates
 
 # Preparation
 .PHONY: init-validations
@@ -12,8 +14,10 @@ prepare:
 # Validation
 .PHONY: build-validations
 build-validations:
-	@echo "Validating artifacts recursively..."
-	$(OSCAL_CLI) validate -f $(DIST_DIR) -e ./src/validations/constraints/fedramp-external-constraints.xml -r
+	@echo "Validating rev5 artifacts recursively..."
+	$(OSCAL_CLI) validate -f $(REV5_BASELINES) -e ./src/validations/constraints/fedramp-external-constraints.xml -r
+	$(OSCAL_CLI) validate -f $(REV5_TEMPLATES) -e ./src/validations/constraints/fedramp-external-constraints.xml -r
+
 test-validations:
 	@echo "Running Cucumber Tests"
 	@npm run test
