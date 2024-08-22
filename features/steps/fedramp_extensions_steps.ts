2fb784ec2883f598c2736f627bf1af8764361012
wandmagic <156969148+wandmagic@users.noreply.github.com>
2024-08-22 09:50:43 -0400

    Update features/steps/fedramp_extensions_steps.ts
    Co-authored-by: David Waltermire <davewaltermire@gmail.com>

--- features/steps/fedramp_extensions_steps.ts
+++ features/steps/fedramp_extensions_steps.ts
@@ -601,7 +601,7 @@ Then("I should have both FAIL and PASS tests for constraint ID {string}", functi
     console.log(`${constraintId}: No tests found`);
     expect.fail(`Constraint ${constraintId} has no tests`);
   } else if (!testCoverage.pass) {
-    console.log(`${constraintId}: Missing positive test`);
+    console.log(`${constraintId}: Missing at least one positive test`);
     expect.fail(`Constraint ${constraintId} is missing a positive test`);
   } else if (!testCoverage.fail) {
     console.log(`${constraintId}: Missing negative test`);
