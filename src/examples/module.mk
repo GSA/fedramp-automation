d5b9ddcf108acf8c846919d35a66d828d6db8e08
A.J. Stein <aj@gsa.gov>
2024-09-09 20:10:22 -0400

    From `docker-compose` to `docker compose` for #643
    More details about GHA runner compatibility with old versus new docker
    compose commands in this discussion post.
    
    https://github.com/orgs/community/discussions/27185

--- src/examples/module.mk
+++ src/examples/module.mk
@@ -3,14 +3,14 @@ test-examples: test-example-java test-example-python test-example-javascript  ##
 test-example-java:  ## Test example Java project
 	@echo "Verifying Java example..."
 	cd src/examples/java && \
-		docker-compose run example mvn test
+		docker compose run example mvn test
 
 test-example-python:  ## Test example Python project
 	@echo "Verifying Python example..."
 	cd src/examples/python && \
-		docker-compose run example pytest
+		docker compose run example pytest
 
 test-example-javascript:  ## Test example Javascript project
 	@echo "Verifying Javascript example..."
 	cd src/examples/javascript && \
-		docker-compose run example ./test.sh
+		docker compose run example ./test.sh
