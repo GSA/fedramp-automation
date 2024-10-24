161bdbe81d86e5e65a28b49e268062d8b9881ba8
~ . ~ <paul.n.wand@gsa.gov>
2024-10-24 12:43:10 -0400

    move index to oscal-external-constraints file

--- features/steps/fedramp_extensions_steps.ts
+++ features/steps/fedramp_extensions_steps.ts
@@ -143,7 +143,6 @@ Given("I have Metaschema extensions documents", function (dataTable) {
   const files = readdirSync(constraintDir);
   metaschemaDocuments = files
     .filter((file) => file.endsWith(".xml")).sort()
-    .filter((x) => !x.startsWith("oscal")) //temporary
     .map((file) => join(constraintDir, file));
 });
 
