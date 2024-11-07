6069076370caff6e0fd9d6494609f2fd8635269d
~ . ~ <paul.n.wand@gsa.gov>
2024-11-07 16:23:15 -0500

    Update fedramp_extensions_steps.ts

--- features/steps/fedramp_extensions_steps.ts
+++ features/steps/fedramp_extensions_steps.ts
@@ -683,7 +683,7 @@ Then('I should verify that all constraints follow the style guide constraint', a
   for (const file_name of constraint_files) {
     const filePath = join(constraintDir, file_name.trim());
     try {
-      const {isValid,log} = await validateDocument(filePath,{flags:['disable-schema'],quiet:true,extensions:[styleGuidePath],module:"http://csrc.nist.gov/ns/oscal/metaschema/1.0"},executor)
+      const {isValid,log} = await validateDocument(filePath,{flags:['disable-schema'],quiet:false,extensions:[styleGuidePath],module:"http://csrc.nist.gov/ns/oscal/metaschema/1.0"},executor)
       writeFileSync(
         join(
           __dirname,
